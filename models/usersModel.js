be7aca094b19a27f55de62e01e3b7e077ad7169f
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-28 13:52:41 -0300

    add model file for users

---
+++ models/usersModel.js
@@ -0,0 +1,75 @@
+import mongoose from 'mongoose';
+import bcrypt from "bcrypt";
+
+
+const usersSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+
+        type: String,
+        required: true,
+        unique: true,
+    },
+    
+
+    password: {
+        type: String,
+        required: true,
+
+    },
+
+    budget: {
+        type: String,
+        required: false,
+        default: '0'
+    },
+
+    img: {
+        type: String,
+        required: false,
+
+    }
+
+},
+
+    {
+        timestamps : true, 
+    }
+
+)
+
+// ====== Hash the password ====== //
+
+usersSchema.pre('save', async function (next) { 
+
+    if(!this.isModified("password")) { // this is for know if the password was hashed
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+
+    this.password = await bcrypt.hash(this.password, salt)
+
+});
+
+
+
+// Check if password is correct
+usersSchema.methods.checkPassword = async function(passForm){
+    
+    return await bcrypt.compare(passForm, this.password)
+}
+
+
+
+const Users = mongoose.model('usersBudget', usersSchema);
+
+
+
+export default Users;
