e0ed76c4571c1bd0dd9a41cf3f40e969c7c4a234
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-29 15:48:29 -0300

    add allowed cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import usersRouter from './routes/usersRouter.js'
 import operationsRouter from './routes/operationsRouter.js'
+import cors from 'cors'
 
 const app = express();
 
@@ -11,6 +12,23 @@ app.use(express.json());
 dotenv.config()
 
 connectDB()
+
+
+const allowedDomains = [process.env.FRONTEND_URL_TOKEN]
+
+const corsOptions = {
+    origin: function(origin, callback){
+        if(allowedDomains.indexOf(origin) !== -1){
+
+            callback(null, true)
+
+        }else{
+            callback(new Error('not allowed by cors'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 
 
